456f4d514e4eefc0b153a755d453d374d809397a
Bibesh Dhital <bibeshdhital@gmail.com>
2020-01-27 15:58:08 +0545

    Connect with mongoDB and handling unhandle promise rejections

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan');
-
-//Route files
-const bootcamps = require("./routes/bootcamps.js");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps.js");
+
 const app = express();
 //Dev logging middleware
-if (process.env.NODE_ENV==='development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //Mount the routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`.yellow.bold)
 );
+
+//Handle unhandle promise rejections
+process.on("unhandledRejection", (err, Promise) => {
+  console.log(`Error:${err.message}`);
+  //Close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
