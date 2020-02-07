16460281f72f1ab10005c6f6507095e4c08b4369
Bibesh Dhital <bibeshdhital@gmail.com>
2020-02-07 21:47:17 +0545

    Handling a method forgotPassword , resetPassword update  password and so on

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
+const crypto = require("crypto");
 const ErrorResponse = require("./../utils/errorResponse");
 const asyncHandler = require("./../middleware/async");
+const sendEmail = require("../utils/sendEmail");
 const User = require("./../models/User");
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,15 +19,15 @@ exports.register = asyncHandler(async (req, res, next) => {
 
 exports.login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
-    
+
   //Validate email and password
   if (!email || !password) {
     return next(new ErrorResponse(`Please provide an email and password`, 400));
   }
-  
+
   //check for user
   const user = await User.findOne({ email }).select("+password");
-  
+
   if (!user) {
     return next(new ErrorResponse(`Invalid email/password`, 401));
   }
@@ -36,6 +38,132 @@ exports.login = asyncHandler(async (req, res, next) => {
   if (!isMatch) {
     return next(new ErrorResponse(`Invalid credentials`, 401));
   }
+
+  sendTokenResponse(user, 200, res);
+});
+//Forgot password
+// route POST/api/v1/auth/forgotpassword
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse(`There is no user with that email`, 404));
+  }
+  //Get rest token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/auth/resetPassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to :\n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password Reset Token",
+      message
+    });
+    res.status(200).json({
+      success: true,
+      data: "Email sent"
+    });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse(`Email could not be sent`, 500));
+  }
+  // res.status(200).json({
+  //   success: true,
+  //   data: user
+  // });
+});
+
+//Get current logged in user
+exports.getMe = asyncHandler(async (req, res, next) => {
+  
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+//Update user details
+//PUT/api/v1/auth/updatedetails
+
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
+  
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+//Update password
+//PUT/api/v1/auth/updatePassword
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  let user = await User.findById(req.user.id).select('+password');
+
+  //Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse(`Password is incorrect`, 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
+
+
+
+//Reset password
+//PUT/api/v1/auth/resetPassword/:resetToken
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  //Get hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {
+      $gt: Date.now()
+    }
+  });
+
+  if (!user) {
+    return next(new ErrorResponse(`Invalid token`, 400));
+  }
+
+  //Set new password
+  user.password = req.body.password;
+  user.resetPasswordToke = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
 
   sendTokenResponse(user, 200, res);
 });
@@ -51,8 +179,8 @@ const sendTokenResponse = (user, statusCode, res) => {
     ),
     httpOnly: true
   };
-  if (process.env.NODE_ENV==='production') {
-    options.secure = true; 
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
   }
   res
     .status(statusCode)
@@ -62,16 +190,3 @@ const sendTokenResponse = (user, statusCode, res) => {
       token
     });
 };
-
-
-//Get current logged in user
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data:user
-  })
-
-
-});
--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -8,11 +8,9 @@ const Bootcamp = require("./../models/Bootcamp");
 //@route Get/api/v1/bootcamps/:bootcampId/courses
 //@access public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  
-
   if (req.params.bootcampId) {
     const courses = await Course.find({ bootcamp: req.params.bootcampId });
-    
+
     return res.status(200).json({
       success: true,
       count: courses.length,
@@ -21,17 +19,16 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   } else {
     res.status(200).json(res.advanceResults);
   }
- 
 });
 
 exports.getCourse = asyncHandler(async (req, res, next) => {
-    const course = await (await Course.findById(req.params.id)).populate({
-        path: 'bootcamp',
-        select:'name description'
-    });
-    if (!course) {
-        return next(new ErrorResponse(`No course with the id of ${req.params.id}`));
-    }
+  const course = await (await Course.findById(req.params.id)).populate({
+    path: "bootcamp",
+    select: "name description"
+  });
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`));
+  }
 
   res.status(200).json({
     success: true,
@@ -40,31 +37,59 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 });
 
 exports.addCourse = asyncHandler(async (req, res, next) => {
-    req.body.bootcamp = req.params.bootcampId;
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
-    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
-    if (!bootcamp) {
-        return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`));
-    }
-    const course = await Course.create(req.body);
-    res.status(201).json({
-        success: true,
-        data: course
-      });
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`)
+    );
+  }
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}
+        .`,
+        401
+      )
+    );
+  }
+
+  const course = await Course.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: course
+  });
 });
 
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-    let course = await Course.findById(req.params.id);
+  let course = await Course.findById(req.params.id);
 
   if (!course) {
-    return next(new ErrorResponse(`No course with the id of ${req.params.id}`),404);
-}
-course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  //Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update a course  ${course._id}
+        .`,
+        401
+      )
+    );
+  }
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
-    runValidators:true
-});
-    
+    runValidators: true
+  });
+
   res.status(200).json({
     success: true,
     data: course
@@ -75,11 +100,24 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id);
 
   if (!course) {
-    return next(new ErrorResponse(`No course with the id of ${req.params.id}`),404);
-}
-    
-    await course.remove();
-    
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+  //Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete a course  ${course._id}
+        .`,
+        401
+      )
+    );
+  }
+
+  await course.remove();
+
   res.status(204).json({
     success: true,
     data: null
