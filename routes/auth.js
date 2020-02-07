a739303954d72912185f2ba4d138c5e95f9e6860
Bibesh Dhital <bibeshdhital@gmail.com>
2020-02-07 21:44:59 +0545

    Handling password reset,updatepassword,forgotPassword and so on

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const { register, login,getMe } = require("./../controllers/authController");
-const { protect } = require('./../middleware/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,updatePassword
+} = require("./../controllers/authController");
+const { protect } = require("./../middleware/auth");
 const router = express.Router();
 
+router.patch("/resetPassword/:resetToken", resetPassword);
+router.patch("/updatedetails",protect, updateDetails);
+router.patch("/updatePassword",protect, updatePassword);
 router
-    .post("/register", register)
-    .post("/login", login)
-    .get('/me',protect,getMe);
+  .post("/register", register)
+  .post("/login", login)
+  .get("/me", protect, getMe)
+  .post("/forgotpassword", forgotPassword);
 
 module.exports = router;
