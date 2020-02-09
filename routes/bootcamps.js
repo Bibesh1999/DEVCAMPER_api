518a0989db588f9551e187894d042f1a7b057a20
Bibesh Dhital <bibeshdhital@gmail.com>
2020-02-09 19:41:42 +0545

    Re-route review router  into other resource router

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -12,11 +12,13 @@ const Bootcamp = require("../models/Bootcamp");
 const advanceResults = require("../middleware/advanceResults");
 const { protect, authorize } = require("./../middleware/auth");
 const courseRouter = require("./courses");
+const reviewRouter = require("./reviews");
 
 const router = express.Router();
 
 //Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
+router.use("/:bootcampId/reviews", reviewRouter);
 
 //route for upload photo
 router
